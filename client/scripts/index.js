216096e9c8fd988480ae372388c8d02b3881668e
xhmyjae <tomatis.mt@gmail.com>
2022-05-11 18:07:58 +0200

    feat: class in js complete

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,51 +1,66 @@
 class questionnaire
 {
+    constructor(questions, inputAnswers, score) {
+        this.questions = questions;
+        this.inputAnswers = inputAnswers;
+        this.score = score;
+    }
 
     getCorrectAnswer(questions)
     {
-        let correctAnswer = {};
+        questions.forEach( () => {
+            let inputs = questions.querySelectorAll('input');
+            let inputsArray = Array.from(inputs);
 
-        for (let i = 0; i < questions.length; i++)
-        {
-            correctAnswer[i] = 0;
+            for (let i = 0; i < inputsArray.length; i++) {
+                if (inputsArray[i].value === 'correct') {
+                    questions[i].push(inputsArray[i]);
+                }
+            }
+        });
+    }
+
+    checkAnswer(questions, inputAnswers)
+    {
+        questions.forEach( () => {
+            let inputs = questions.querySelectorAll('input');
+            let inputsArray = Array.from(inputs);
+
+            for (let i = 0; i < inputsArray.length; i++) {
+                if (inputsArray[i].value === 'correct' && inputsArray[i].checked) {
+                    inputAnswers[i].push(inputsArray[i]);
+                }
+            }
+        });
+    }
+
+    calculateScore(questions, inputAnswers)
+    {
+        for (let i = 0; i < questions.length; i++) {
+            if (inputAnswers[i].length === questions[i].length) {
+                this.score++;
+            }
         }
-
-        questions.forEach( question =>
-        {
-
-        });
-
-        questions.forEach(answer => {
-            let indexAswer = answers.indexOf(answer)
-            let input = answer.querySelectorAll('input');
-            input.forEach(input => {
-               if (input.value === 'correct') {
-                   correctAnswer++;
-               }
-            });
-        });
-
-        return correctAnswer;
     }
+}
 
-    checkAnswer(question, answers, correctAnswer)
-    {
-        let answers = document.querySelectorAll('.answers');
-        let userCorrectAnswer = 0;
+let questions = document.querySelectorAll('.questions');
+let next = document.querySelector('.next-button');
+let previous = document.querySelector('.previous-button');
 
-        answers.forEach(answer => {
-           let input = answer.querySelectorAll('input');
-           input.forEach(input => {
-              if (input.checked) {
-                  userCorrectAnswer++;
-              }
-           });
-        });
-    }
+next.addEventListener('click', e => {
+   let currentQuestion = e.target;
 
-    incrementScore(score)
-    {
+   let question = document.querySelector('.questions:not(.hide)');
+});
 
-    }
+questions.forEach(question => {
+    let questionnaire= new questionnaire(question);
+    questionnaire.getCorrectAnswer(question);
+    questionnaire.checkAnswer(question);
+    let score = questionnaire.incrementScore(score);
+});
 
+function submitAnswers() {
+    const total = questionnaire.getCorrectAnswer(questions);
 }
