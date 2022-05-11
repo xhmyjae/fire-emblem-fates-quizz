9e12058c723b45ac1930759e830dc00cd5f4e6c4
xhmyjae <tomatis.mt@gmail.com>
2022-05-11 11:29:35 +0200

    feat: js not working + css done

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,19 +1,50 @@
 class questionnaire
 {
 
-    multipleChoices(question, answers, correctAnswer) {
+    getCorrectAnswer(questions)
+    {
+        let correctAnswer = {};
 
+        for (let i = 0; i < questions.length; i++)
+        {
+            correctAnswer[i] = 0;
+        }
+
+        questions.forEach( question =>
+        {
+
+        });
+
+        questions.forEach(answer => {
+            let indexAswer = answers.indexOf(answer)
+            let input = answer.querySelectorAll('input');
+            input.forEach(input => {
+               if (input.value === 'correct') {
+                   correctAnswer++;
+               }
+            });
+        });
+
+        return correctAnswer;
     }
 
-    inputChoice(question, answers, correctAnswer) {
+    checkAnswer(question, answers, correctAnswer)
+    {
+        let answers = document.querySelectorAll('.answers');
+        let userCorrectAnswer = 0;
 
+        answers.forEach(answer => {
+           let input = answer.querySelectorAll('input');
+           input.forEach(input => {
+              if (input.checked) {
+                  userCorrectAnswer++;
+              }
+           });
+        });
     }
 
-    soloChoice(question, answers, correctAnswer) {
-
-    }
-
-    incrementScore(score) {
+    incrementScore(score)
+    {
 
     }
 
